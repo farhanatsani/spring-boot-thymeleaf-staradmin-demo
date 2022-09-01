91cb13fd0687b8b5c9444158ff5d14f36aeaa331
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-01 07:35:45 +0700

    #21 - Bug Fix code :pencil2: :blue_book: :whale:

--- src/main/java/com/farhan/staradmin/configuration/MvcConfiguration.java
+++ src/main/java/com/farhan/staradmin/configuration/MvcConfiguration.java
@@ -1,6 +1,6 @@
 package com.farhan.staradmin.configuration;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
