2950e182620500802effbbf8d5dc29e21149bdb7
kim chunghwan <keii2k84@gmail.com>
2023-10-08 08:21:17 +0900

    member api

--- src/main/java/com/farhan/staradmin/model/Member.java
+++ src/main/java/com/farhan/staradmin/model/Member.java
@@ -1,22 +1,11 @@
 package com.farhan.staradmin.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class Member {
     Integer id;
     String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
