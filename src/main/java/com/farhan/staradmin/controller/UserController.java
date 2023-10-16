d3756e7b701a76ac12b82280f0e6041af94c0efd
kim chunghwan <keii2k84@gmail.com>
2023-10-16 18:32:43 +0900

    user

---
+++ src/main/java/com/farhan/staradmin/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.farhan.staradmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "pages")
+public class UserController {
+
+
+    @GetMapping(value = "user")
+    public ModelMap mmDashboard() {
+        // todo get apple stock price
+        String test = "test";
+        ModelMap modelMap = new ModelMap();
+        modelMap.addAttribute("key", "Total Revenue");
+        System.out.println("test: " + modelMap.toString());
+        return modelMap;
+    }
+}
